f0620dbbed6ccdb24fab6be7f6a1d2b11d13c22b
TheBigCheese <13ros27@kesgmail.net>
2022-07-02 18:04:25 +0100

    Extended the timeout

--- src/current_monitor.rs
+++ src/current_monitor.rs
@@ -104,10 +104,11 @@ pub struct CurrentMonitor<const N: usize> {
 
 impl<const N: usize> CurrentMonitor<N> {
     pub fn default() -> Result<CurrentMonitor<N>, serial_core::Error> {
-        let mut port = TTYPort::open(Path::new("/dev/ttyAMA0")).unwrap();
-        let mut settings = port.read_settings().unwrap();
-        settings.set_baud_rate(BaudRate::Baud38400).unwrap();
-        port.write_settings(&settings).unwrap();
+        let mut port = TTYPort::open(Path::new("/dev/ttyAMA0"))?;
+        let mut settings = port.read_settings()?;
+        settings.set_baud_rate(BaudRate::Baud38400)?;
+        port.write_settings(&settings)?;
+        port.set_timeout(std::time::Duration::from_secs(10))?;
         Ok(CurrentMonitor { port })
     }
 
