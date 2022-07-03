b5fb9f6469a91504ab4966f3d565067c08873974
TheBigCheese <13ros27@kesgmail.net>
2022-07-03 13:20:19 +0100

    Added file switching each day

--- power_manager.py
+++ power_manager.py
@@ -64,14 +64,15 @@ class DataLogger:
             mkdir(folder)
         self.freq = freq
         self.folder = folder
+        self.names = names
         self.current_types = types
         self.start_time = time.time()
-        self._new_file(names)
+        self._new_file()
         self.last_tick = None
 
-    def _new_file(self, names: [str]):
-        day = self.start_time // 86400
-        root_filename = f'D{int(day)}'
+    def _new_file(self):
+        self.day = int(time.time() // 86400)
+        root_filename = f'D{self.day}'
         i = 1
         while True:
             if i == 1:
@@ -82,8 +83,9 @@ class DataLogger:
             if not path.is_file():
                 self.fp = path
                 with open(path, 'x') as fp:
-                    mes = f'Time' + ''.join([f',{n}({t.name})' for (n, t) in
-                                             zip(names, self.current_types)])
+                    mes = 'Time'
+                    mes += ''.join([f',{n}({t.name})' for (n, t) in
+                                    zip(self.names, self.current_types)])
                     fp.write(mes + '\n')
                 break
             i += 1
@@ -93,6 +95,8 @@ class DataLogger:
         this_tick = time.time() // self.freq
         if self.last_tick is None or self.last_tick < this_tick:
             self.last_tick = this_tick
+            if int(time.time() // 86400) != self.day:
+                self._new_file()
             self._log_to_file(currents)
 
     def _log_to_file(self, currents: [Current]):
