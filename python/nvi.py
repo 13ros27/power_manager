b987a9d403235afbcab6eb0d3444f1fa055319e9
TheBigCheese <13ros27@kesgmail.net>
2022-07-04 19:41:09 +0100

    Made __getitem__ error if chat doesn't exist

--- python/nvi.py
+++ python/nvi.py
@@ -20,7 +20,7 @@ class NonVolatileInformation:
 
     def __getitem__(self, chat_id: int) -> dict:
         """Get the information about a given chat."""
-        return self._info['chats'].get(str(chat_id))
+        return self._info['chats'][str(chat_id)]
 
     def setitem(self, chat_id: int, setting: str, new_val):
         """Set a piece of information about a given chat."""
