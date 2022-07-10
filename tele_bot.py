5b4b7712c859163dcc5ad518594ebdc777b503e1
William Rose <william@theroses.me.uk>
2022-07-10 18:12:13 +0100

    Fixed logging

--- tele_bot.py
+++ tele_bot.py
@@ -115,7 +115,7 @@ class TelegramBot:
         """Update what it knows about the state."""
         self.last_info = self.info
         self.info = (currents, estimated, recommended)
-        self.logger.warning(self.info, self.last_info)
+        self.logger.warning(f'{self.info}, {self.last_info}')
         to_update = set()
         for (i, handlers) in enumerate(self.change_handlers):
             if self.info[i] != self.last_info[i]:
