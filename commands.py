fc50f1156b390c8861702f5bd6d1a33426217e81
William Rose <william@theroses.me.uk>
2022-07-10 17:46:55 +0100

    Fixed more naming in @password

--- commands.py
+++ commands.py
@@ -9,8 +9,8 @@ from quasar import Quasar
 
 def password(f):
     """Check if this chat has entered the correct password."""
-    def wrapper(self, update: Update, context: CallbackContext):
-        if self.tele_bot.nvinfo.is_valid(self.tele_bot.get_chat_id(update)):
+    def wrapper(self: TeleCommands, update: Update, context: CallbackContext):
+        if self.tbot.nvinfo.is_valid(self.tbot.get_chat_id(update)):
             f(self, update, context)
     return wrapper
 
