11585ddd6be386ab60e190385e7bf40a488a9783
William Rose <william@theroses.me.uk>
2022-08-05 12:53:09 +0100

    Changed the StateSelect system a bit with an eye on the future

--- state.py
+++ state.py
@@ -31,36 +31,37 @@ class StateSelect:
             self.power_overflow = False
         self.mode = mode
 
+    def _get_state_normal(self):
+        if timing.past_this_time(self.config.night_start) and not timing.past_this_time(self.config.night_end):
+            soc = self.quasar.soc
+            if soc <= self.config.min_charge:
+                self.remained_overflowed = True
+            if soc < self.config.max_charge:
+                return State.MAX_CHARGE
+            else:
+                return State.PRESERVE
+        else:
+            if not self.remained_overflowed:
+                self.power_overflow = False
+            self.remained_overflowed = False
+            if self.quasar.soc <= self.config.min_charge:
+                self.power_overflow = True
+                return State.PRESERVE
+            else:
+                if self.power_overflow:
+                    return State.WINTER
+                else:
+                    return State.SUMMER
+
+    def _get_state(self, mode: Mode):
+        try:
+            return State(mode.value)
+        except ValueError:
+            if mode == Mode.NORMAL:
+                return self._get_state_normal()
+            else:
+                raise ValueError(f"Unexpected mode {mode}")
+
     @property
     def state(self) -> State:
-        if self.mode == Mode.MAX_CHARGE:
-            return State.MAX_CHARGE
-        elif self.mode == Mode.SUMMER:
-            return State.SUMMER
-        elif self.mode == Mode.WINTER:
-            return State.WINTER
-        elif self.mode == Mode.LOW_CHARGE:
-            return State.LOW_CHARGE
-        elif self.mode == Mode.NORMAL:
-            soc = self.quasar.soc
-            if timing.past_this_time(self.config.night_start) and not timing.past_this_time(self.config.night_end):
-                if soc <= self.config.min_charge:
-                    self.remained_overflowed = True
-                if soc < self.config.max_charge:
-                    return State.MAX_CHARGE
-                else:
-                    return State.PRESERVE
-            else:
-                if not self.remained_overflowed:
-                    self.power_overflow = False
-                self.remained_overflowed = False
-                if soc <= self.config.min_charge:
-                    self.power_overflow = True
-                    return State.PRESERVE
-                else:
-                    if self.power_overflow:
-                        return State.WINTER
-                    else:
-                        return State.SUMMER
-        else:
-            return State.PRESERVE
+        return self._get_state(self.mode)
