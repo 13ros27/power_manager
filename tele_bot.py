9318443015008f581f938cda2462e55120c85ff2
William Rose <william@theroses.me.uk>
2022-12-03 12:44:36 +0000

    You can't do empty returns in python

--- tele_bot.py
+++ tele_bot.py
@@ -328,7 +328,7 @@ class TelegramBot:
 
     def message_handler(self, update: Update, _: CallbackContext):
         if self.particular_message_handler is None:
-            return
+            return None
         else if update.effective_chat.id == self.particular_message_handler[1]:
             m_handler = self.particular_message_handler[0]
             text, success = m_handler(update.message.text)
